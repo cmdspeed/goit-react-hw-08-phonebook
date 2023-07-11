8289c110593ad0ecfd3508c62aaa914f63795d11
cmdspeed <cmdspeed@gmail.com>
2023-07-11 18:08:59 +0200

    delete not used file

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,11 +3,3 @@ export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 export const selectUser = state => state.auth.user;
 
 export const selectIsRefreshing = state => state.auth.isRefreshing;
-
-///edit
-
-export const getIsLoggedIn = state => state.auth.isLoggedIn;
-
-export const getUsername = state => state.auth.user.name;
-
-export const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
--- src/redux/store.js
+++ src/redux/store.js
@@ -5,13 +5,12 @@ import { authReducer } from './auth/slice';
 import { persistStore, persistReducer } from 'redux-persist';
 
 import storage from 'redux-persist/lib/storage';
-// delete V
+
 const authPersistConfig = {
   key: 'auth',
   storage,
   whitelist: ['token'],
 };
-//^^ delete
 
 export const store = configureStore({
   reducer: {
